e02833e27c5bcae4a98d175f84d7d34cc2979e55
Franca Bauer <frb@dmi.dk>
2024-11-12 16:32:46 +0100

    reading in, converting and saving DTU Space Greenland Data - Not fully working yet

--- source/DTU_space_data_to_dba.py
+++ source/DTU_space_data_to_dba.py
@@ -42,10 +42,10 @@ class DataConverter():
 
         if '_cond' in base_filename:
             measurement_name = 'Conductivity'
-        elif '_height' in base_filename:
+        elif '_height.' in base_filename:
             measurement_name = 'Height'
-        elif '_height_1013' in base_filename:
-            measurement_name = 'Height (assumed pressure)'
+        elif '_height_1013.' in base_filename:
+            measurement_name = 'Height(assumed pressure)'
         elif '_press' in base_filename:
             measurement_name = 'Pressure'
         elif '_salin' in base_filename:
@@ -58,11 +58,22 @@ class DataConverter():
         return measurement_name
     
     def run(self):
+        self.load_data()
+        df = self.convert_data_to_df()
+        #sort df by time of measurements
+        df = df.sort_values(by='timestamp')
+        #fix time as first column and error_flag as last colum
+        final_df = df[['timestamp'] + [col for col in df.columns if col not in ['timestamp', 'error_flag']] + ['error_flag']]
+        self.export_to_dba(final_df)
+
+    def export_to_dba(self, df):
+        df.to_csv(os.path.join(self.output_path, f'{self.station}_data.dba'), sep=' ', index=False)
+
+    def load_data(self):
 
         # Iterate through each file in the directory
         for file_name in os.listdir(self.data_path):
             # Process only .zip files
-            print(file_name)
             if file_name.lower().endswith('.zip'):
                 # Get the full path of the .zip file
                 zip_path = os.path.join(self.data_path, file_name)
@@ -72,21 +83,69 @@ class DataConverter():
                     for zip_info in zip_file.infolist():
                         # Check if it's a .txt file and starts with the desired prefix
                         base_filename = os.path.basename(zip_info.filename)
-                        print(base_filename)
                         if base_filename.endswith('.txt') and base_filename.startswith(self.file_prefix):
                             if '_met_' in base_filename:
                                 continue
                             else:
                                 # Open and read the content of the .txt file
                                 with zip_file.open(zip_info) as txt_file:
+                                    print(f"Loaded DataFrame from {zip_info.filename} in {file_name}")
                                     #Extract which measurement it is
                                     measurement_name = self.get_measured_parameter(zip_path, base_filename)
-                                    # Process the content of the .txt file
-                                    monthly_df = pd.read_csv(txt_file, sep='\s+', header=None, names=['date', 'time', measurement_name, 'error_flag'])
+                                    #check formate of the txt.file
+                                    with zip_file.open(zip_info) as txt_file2:
+                                        df = pd.read_csv(txt_file2, sep='\s+', nrows=1)
+                                    file_columns = set(df.columns)
+                                    if len(file_columns) == 4:
+                                        #Extract measurements the content of the .txt file
+                                        monthly_df = pd.read_csv(txt_file, sep='\s+', header=None, names=['date', 'time', measurement_name, 'error_flag'])
+                                        monthly_df['timestamp'] = pd.to_datetime(monthly_df['date'] + ' ' + monthly_df['time']).dt.strftime('%Y%m%d%H%M%S')
+                                        self.data_df.append(monthly_df)
+                                        del monthly_df['date']
+                                        del monthly_df['time']
+                                    elif len(file_columns) == 3:
+                                        #Extract measurements the content of the .txt file
+                                        monthly_df = pd.read_csv(txt_file, sep='\s+', header=None, names=['date', 'time', measurement_name])
+                                        monthly_df['timestamp'] = pd.to_datetime(monthly_df['date'] + ' ' + monthly_df['time']).dt.strftime('%Y%m%d%H%M%S')
+                                        monthly_df['error_flag'] = np.nan
+                                        del monthly_df['date']
+                                        del monthly_df['time']
+                                    elif len(file_columns)==2:
+                                        monthly_df = pd.read_csv(txt_file, sep=',', header=None, names=['timestamp', measurement_name])
+                                        monthly_df['timestamp'] = pd.to_datetime(monthly_df['timestamp'], format='%Y/%m/%d-%H:%M')
+                                        monthly_df['timestamp'] = monthly_df['timestamp'].dt.strftime('%Y%m%d%H%M%S')
+                                        monthly_df['error_flag'] = np.nan
+                                    else:
+                                        raise Exception('Content of the .txt a format which is not compatibel with the current code version.')
                                     self.data_df.append(monthly_df)
-                                    print(f"Loaded DataFrame from {zip_info.filename} in {file_name}")
+                                    
+    def convert_data_to_df(self):
+        # Initialize a dataframe with the first dataframe from the list
+        result_df = self.data_df[0]
 
-        # Combine all DataFrames into one and save it
-        combined_df = pd.concat(self.data_df, ignore_index=True)
-        combined_df.to_csv(os.path.join(self.output_path, f'{self.station}_data.dba'), index=False)
-        
+        # Iterate over each subsequent dataframe in the list
+        for df in self.data_df[1:]:
+            # Merge matching rows
+            common_columns = result_df.columns.intersection(df.columns)
+            if len(common_columns)== 2:
+                merged_df = pd.merge(result_df, df, on=list(common_columns), how='inner')
+            elif len(common_columns) == 3:
+                #Sort that common columns are always in the same order
+                unknown_column = [col for col in common_columns if col not in ['error_flag', 'timestamp']][0]
+                df = df[['error_flag', 'timestamp', unknown_column]]
+                df_only = df[~df['timestamp'].isin(result_df['timestamp'])]
+                if df_only.empty:
+                    # Merge df_A and df_B on 'error_flag' and 'timestamp', using a left join to retain all rows from df_A
+                    merged_df = pd.merge(result_df, df[list(common_columns)], on=[df.columns[1], df.columns[0]], how='left', suffixes=('', '_new'))
+                    # Fill NaNs in the value column from big df with the values from the measurement df
+                    merged_df[df.columns[2]] = merged_df[df.columns[2]].fillna(merged_df[f'{df.columns[2]}_new'])
+                    # Drop the extra column
+                    merged_df.drop(columns=[f'{df.columns[2]}_new'], inplace=True)
+                else:
+                    merged_df = pd.merge(result_df, df[list(common_columns)], on=[list(common_columns)[1], list(common_columns)[0], list(common_columns)[2]], how='outer')
+            else:
+                raise Exception('Dfs are in a format which is not compatibel with the current code version.')
+            
+            result_df = merged_df
+
+        return result_df
--- tests/test_greenland_dba_generator.py
+++ tests/test_greenland_dba_generator.py
@@ -42,5 +42,95 @@ class Test_DTUSpace_DataConverter(unittest.TestCase):
         data_conversion.set_relev_station(station)
         data_conversion.run()
 
+    def test_data_Qaqortoq_conversion(self):
+       
+        print(os.getcwd())
+
+        #select the station (here: Qaqortoq)
+        index_station=0
+        station = self.stations[index_station]
+
+        #select output folder
+        output_path = os.path.join(os.getcwd(),'output', station)
+        if Path(output_path).exists(): shutil.rmtree(Path(output_path))
+
+        data_conversion = data_converter.DataConverter()
+        data_conversion.set_output_path(output_path)
+        data_conversion.load_data_path(self.datadir)
+        data_conversion.set_relev_station(station)
+        data_conversion.run()
+
+    def test_data_Nuuk1_conversion(self):
+       
+        print(os.getcwd())
+
+        #select the station (here: Nuuk1)
+        index_station=3
+        station = self.stations[index_station]
+
+        #select output folder
+        output_path = os.path.join(os.getcwd(),'output', station)
+        if Path(output_path).exists(): shutil.rmtree(Path(output_path))
+
+        data_conversion = data_converter.DataConverter()
+        data_conversion.set_output_path(output_path)
+        data_conversion.load_data_path(self.datadir)
+        data_conversion.set_relev_station(station)
+        data_conversion.run()
+
+    def test_data_Nuuk_conversion(self):
+       
+        print(os.getcwd())
+
+        #select the station (here: Nuuk)
+        index_station=2
+        station = self.stations[index_station]
+
+        #select output folder
+        output_path = os.path.join(os.getcwd(),'output', station)
+        if Path(output_path).exists(): shutil.rmtree(Path(output_path))
+
+        data_conversion = data_converter.DataConverter()
+        data_conversion.set_output_path(output_path)
+        data_conversion.load_data_path(self.datadir)
+        data_conversion.set_relev_station(station)
+        data_conversion.run()
+
+    def test_data_Pituffik_conversion(self):
+       
+        print(os.getcwd())
+
+        #select the station (here: Pituffik)
+        index_station=4
+        station = self.stations[index_station]
+
+        #select output folder
+        output_path = os.path.join(os.getcwd(),'output', station)
+        if Path(output_path).exists(): shutil.rmtree(Path(output_path))
+
+        data_conversion = data_converter.DataConverter()
+        data_conversion.set_output_path(output_path)
+        data_conversion.load_data_path(self.datadir)
+        data_conversion.set_relev_station(station)
+        data_conversion.run()
+
+    def test_data_Ittoqqortoormiit_conversion(self):
+       
+        print(os.getcwd())
+
+        #select the station (here: Ittoqqortoormiit)
+        index_station=1
+        station = self.stations[index_station]
+
+        #select output folder
+        output_path = os.path.join(os.getcwd(),'output', station)
+        if Path(output_path).exists(): shutil.rmtree(Path(output_path))
+
+        data_conversion = data_converter.DataConverter()
+        data_conversion.set_output_path(output_path)
+        data_conversion.load_data_path(self.datadir)
+        data_conversion.set_relev_station(station)
+        data_conversion.run()
+
 if __name__ == '__main__':
     unittest.main()
